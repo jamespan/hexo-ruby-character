d2012a18c26966bfb8b897f7b1a9601dfda83620
panjiabang <panjiabang@gmail.com>
2015-06-21 14:12:01 +0800

    just work

---
+++ index.js
@@ -0,0 +1,19 @@
+var pinyin = require("pinyin");
+
+hexo.extend.tag.register('ruby', function(args, option) {
+
+  var splited = args.join(' ').split('|');
+
+  var origin = splited[0].trim();
+
+  var ruby = origin;
+  if (splited.length > 1) {
+  	ruby = splited[1].trim();
+  }
+
+  var pinyinic_ruby = [].concat.apply([],pinyin(ruby));
+
+  var ruby_result = "<ruby>" + origin + "<rp>（</rp><rt>" + pinyinic_ruby.join(' ') + "</rt><rp>）</rp></ruby>"
+  
+  return ruby_result;
+});
